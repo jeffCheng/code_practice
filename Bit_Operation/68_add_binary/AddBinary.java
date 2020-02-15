afbf1ea8d6eb230a145891a8b378119626613925
Jeff Cheng <jeff32191182@gmail.com>
2020-02-15 15:52:31 -0700

    add add binary code.

---
+++ Bit_Operation/68_add_binary/AddBinary.java
@@ -0,0 +1,39 @@
+
+public class AddBinary {
+
+	public static void main(String[] args) {
+		System.out.println(addBinary("0", "1"));
+	}
+
+	public static String addBinary(String a, String b) {
+		StringBuilder sb = new StringBuilder();
+		int i = a.length() - 1;
+		int j = b.length() - 1;
+		int carry = 0;
+		while (i >= 0 || j >= 0) {
+			int sum = 0;
+
+			if (i >= 0 && a.charAt(i) == '1') {
+				sum++;
+			}
+			if (j >= 0 && b.charAt(j) == '1') {
+				sum++;
+			}
+			sum += carry;
+			if (sum >= 2) {
+				carry = 1;
+			} else {
+				carry = 0;
+			}
+			sb.insert(0, (char) ((sum % 2) + '0'));
+			System.out.println((char) ((sum % 2) + '0'));
+			i--;
+			j--;
+		}
+		if (carry == 1)
+			sb.insert(0, '1');
+
+		return sb.toString();
+	}
+
+}
